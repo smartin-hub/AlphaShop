651a1174f7497b83e81b74535f9969681bf94207
smartin-hub <smartin@invallee.it>
2021-02-01 10:57:20 +0100

    Sez. 14 - lez. 150

--- src/app/directives/copyright.directive.ts
+++ src/app/directives/copyright.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appCopyright]'
 })
 export class CopyrightDirective {
 
-  constructor() { }
+  constructor(el: ElementRef, renderer: Renderer2) {
+    
+    renderer.addClass(el.nativeElement, 'copyright');
+
+    renderer.setProperty(
+      el.nativeElement,
+      'textContent',
+      `&copy; ${new Date().getFullYear()} by Xantrix Inc.` 
+    );
+  }
 
 }
